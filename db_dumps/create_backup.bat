fdf2505cd678c3965ced908bcce260c8fbc34ee1
Vlasov R. V <radislavvlasov@gmail.com>
2023-07-07 22:33:16 +0300

    feat: добавлен скрипт для генерации

--- db_dumps/create_backup.bat
+++ db_dumps/create_backup.bat
@@ -6,6 +6,7 @@ set PORT=5432
 set DATABASE=wildberries_parser_1
 
 For /f "tokens=1-4 delims=/." %%a in ('date /t') do (set DATE=%%a_%%b_%%c)
+set DATE=%DATE:~0,-1%
 For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set TIME=%%a_%%b)
 set DATETIME=%DATE%_%TIME%
 echo datetime is %DATETIME%
