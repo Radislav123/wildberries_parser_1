6e51a9debb7d12c327bef5c15667d3c10550dd0e
Vlasov R. V <radislavvlasov@gmail.com>
2023-07-07 20:17:46 +0300

    fix: исправлены статичные файлы

--- parser_project/settings.py
+++ parser_project/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 
 from core.settings import Settings
+import os
 
 
 settings = Settings()
@@ -32,8 +33,6 @@ ALLOWED_HOSTS = []
 
 # Application definition
 INSTALLED_APPS = [
-    "django.contrib.staticfiles",
-
     "parser_price",
     "parser_position",
     "core",
@@ -45,6 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    "django.contrib.staticfiles",
 ]
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -97,6 +97,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
