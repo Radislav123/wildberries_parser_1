7cefbed24d7919458003a58d26c91f66e760f880
Vlasov R. V <radislavvlasov@gmail.com>
2023-07-20 15:47:48 +0300

    feat: добавлена ссылка на товар в оповещение

--- bot_telegram/bot.py
+++ bot_telegram/bot.py
@@ -10,6 +10,9 @@ from . import settings
 
 
 class Bot(telebot.TeleBot):
+    class ParseModes:
+        MARKDOWN = "MarkdownV2"
+
     settings = settings.Settings()
 
     def __init__(self, token: str = None):
@@ -28,12 +31,20 @@ class Bot(telebot.TeleBot):
         admin.telegram_chat_id = message.chat.id
         admin.save()
 
-        text = "Вы подписаны на изменения цен.\nЗакройте окно командной строки, открытое для авторизации."
+        text = "Вы подписаны на изменения цен."
         self.send_message(message.from_user.id, text)
 
     @staticmethod
     def cross_out(data: Any) -> str:
         return "".join(['\u0336'.join(str(data)), '\u0336', ' '])
+
+    @staticmethod
+    def escape(string: str) -> str:
+        chars_to_escape = ['_', '*', '[', ']', '(', ')', '~', '`', '>', '#', '+', '-', '=', '|', '{', '}', '.', '!']
+
+        for char in chars_to_escape:
+            string = string.replace(char, '\\' + char)
+        return string
 
     def notify_prices_changed(
             self,
@@ -41,24 +52,28 @@ class Bot(telebot.TeleBot):
             changed_prices: list[tuple[parser_price_models.Price, parser_price_models.Price, float, int]]
     ) -> None:
         for new_price, old_price, price_changing, personal_sale_changing in changed_prices:
+            # [(escape_bool, string)]
             text = []
 
             if price_changing != 0:
-                text.append(f"🟪 Изменилась цена")
+                text.append((True, f"🟪 Изменилась цена"))
             if personal_sale_changing != 0:
-                text.append(f"🟦 Изменилась СПП")
-            text.append("")
+                text.append((True, f"🟦 Изменилась СПП"))
+            text.append((True, ""))
 
             if new_price.item.category is not None:
                 text.append(
-                    f"{new_price.item.category.get_field_verbose_name('name')}: {new_price.item.category.name}",
+                    (True, f"{new_price.item.category.get_field_verbose_name('name')}: {new_price.item.category.name}"),
                 )
             text.extend(
                 [
-
-                    f"{new_price.item.get_field_verbose_name('vendor_code')}: {new_price.item.vendor_code}",
-                    f"{new_price.item.get_field_verbose_name('name')}: {new_price.item.name}",
-                    ""
+                    (
+                        False,
+                        f"{new_price.item.get_field_verbose_name('vendor_code')}: [{new_price.item.vendor_code}]"
+                        f"(https://www.wildberries.ru/catalog/{new_price.item.vendor_code}/detail.aspx)"
+                    ),
+                    (True, f"{new_price.item.get_field_verbose_name('name')}: {new_price.item.name}"),
+                    (True, "")
                 ]
             )
 
@@ -69,21 +84,28 @@ class Bot(telebot.TeleBot):
             if price_changing > 0:
                 text.extend(
                     [
-                        "🟥",
+                        (True, "🟥"),
                         # todo: добавить хранение и парсинг валюты
-                        f"{price_emoji} {price_name}: {new_price_string} <=== {old_price_string} +{price_changing} ₽",
+                        (
+                            True,
+                            f"{price_emoji} {price_name}: {new_price_string} <==="
+                            f" {old_price_string} +{price_changing} ₽"
+                        ),
                     ]
                 )
             elif price_changing < 0:
                 text.extend(
                     [
-                        "🟩",
-                        f"{price_emoji} {price_name}: {new_price_string} <=== {old_price_string} {price_changing} ₽",
+                        (True, "🟩"),
+                        (
+                            True,
+                            f"{price_emoji} {price_name}: {new_price_string} <=== {old_price_string} {price_changing} ₽"
+                        ),
                     ]
                 )
             else:
-                text.append(f"{price_emoji} {price_name}: {new_price_string} ₽", )
-            text.append("")
+                text.append((True, f"{price_emoji} {price_name}: {new_price_string} ₽"))
+            text.append((True, ""))
 
             personal_sale_emoji = '🧮'
             personal_sale_name = new_price.get_field_verbose_name('personal_sale')
@@ -92,20 +114,38 @@ class Bot(telebot.TeleBot):
             if personal_sale_changing > 0:
                 text.extend(
                     [
-                        "🟩",
-                        f"{personal_sale_emoji} {personal_sale_name}: {new_personal_sale_string}"
-                        f" <=== {old_personal_sale_string} +{personal_sale_changing} %",
+                        (True, "🟩"),
+                        (
+                            True,
+                            f"{personal_sale_emoji} {personal_sale_name}: {new_personal_sale_string}"
+                            f" <=== {old_personal_sale_string} +{personal_sale_changing} %"
+                        ),
                     ]
                 )
             elif personal_sale_changing < 0:
                 text.extend(
                     [
-                        "🟥",
-                        f"{personal_sale_emoji} {personal_sale_name}: {new_personal_sale_string}"
-                        f" <=== {old_personal_sale_string} {personal_sale_changing} %",
+                        (True, "🟥"),
+                        (
+                            True,
+                            f"{personal_sale_emoji} {personal_sale_name}: {new_personal_sale_string}"
+                            f" <=== {old_personal_sale_string} {personal_sale_changing} %"
+                        ),
                     ]
                 )
             else:
-                text.append(f"{personal_sale_emoji} {personal_sale_name}: {new_personal_sale_string} %")
+                text.append((True, f"{personal_sale_emoji} {personal_sale_name}: {new_personal_sale_string} %"))
 
-            self.send_message(new_price.item.user.telegram_chat_id, "\n".join(text))
+            escaped_text = []
+            for string in text:
+                if string[0]:
+                    escaped_text.append(self.escape(string[1]))
+                else:
+                    escaped_text.append(string[1])
+
+            self.send_message(
+                new_price.item.user.telegram_chat_id,
+                "\n".join(escaped_text),
+                parse_mode = self.ParseModes.MARKDOWN,
+                disable_web_page_preview = True
+            )
