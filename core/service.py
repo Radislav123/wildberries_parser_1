2d1b5e668f26420231b8efc8b2d39db704292a7c
Vlasov R. V <radislavvlasov@gmail.com>
2023-10-03 18:45:21 +0300

    fix: слишком большие запросы цен теперь дробятся на серию меньших размеров

--- core/service.py
+++ core/service.py
@@ -2,9 +2,9 @@ import time
 
 import requests
 from requests import JSONDecodeError
-from logger import Logger
 
 from core.settings import Settings
+from logger import Logger
 
 
 settings = Settings()
@@ -18,34 +18,38 @@ def parse_prices(
 ) -> tuple[dict[int, dict[str, int | float | str]], dict[int, Exception]]:
     # если указать СПП меньше реальной, придут неверные данные, при СПП >= 100 данные не приходят
     request_personal_sale = 99
-    url = (f"https://card.wb.ru/cards/detail?appType=1&curr=rub"
-           f"&dest={dest}&regions={regions}&spp={request_personal_sale}"
-           f"&nm={';'.join([str(x) for x in vendor_codes])}")
-    items_response = requests.get(url)
-
-    item_dicts = {x["id"]: x for x in items_response.json()["data"]["products"]}
+    chunk_size = 100
+    chunks = [vendor_codes[x: x + chunk_size] for x in range(0, len(vendor_codes), chunk_size)]
     prices = {}
     errors = {}
-    for vendor_code in vendor_codes:
-        try:
-            item_dict: dict = item_dicts[vendor_code]
-            price, final_price, personal_sale, sold_out = get_price(item_dict)
-            reviews_amount = int(item_dict["feedbacks"])
-            category_name = get_category_name(vendor_code)
-            name_site = f"{item_dict['brand']} / {item_dict['name']}"
 
-            prices[vendor_code] = {
-                "price": price,
-                "final_price": final_price,
-                "personal_sale": personal_sale,
-                # todo: add sold_out to model
-                "sold_out": sold_out,
-                "reviews_amount": reviews_amount,
-                "category_name": category_name,
-                "name_site": name_site
-            }
-        except Exception as error:
-            errors[vendor_code] = error
+    for vendor_codes_chunk in chunks:
+        url = (f"https://card.wb.ru/cards/detail?appType=1&curr=rub"
+               f"&dest={dest}&regions={regions}&spp={request_personal_sale}"
+               f"&nm={';'.join(str(x) for x in vendor_codes_chunk)}")
+        items_response = requests.get(url)
+
+        item_dicts = {x["id"]: x for x in items_response.json()["data"]["products"]}
+        for vendor_code in vendor_codes_chunk:
+            try:
+                item_dict: dict = item_dicts[vendor_code]
+                price, final_price, personal_sale, sold_out = get_price(item_dict)
+                reviews_amount = int(item_dict["feedbacks"])
+                category_name = get_category_name(vendor_code)
+                name_site = f"{item_dict['brand']} / {item_dict['name']}"
+
+                prices[vendor_code] = {
+                    "price": price,
+                    "final_price": final_price,
+                    "personal_sale": personal_sale,
+                    # todo: add sold_out to model
+                    "sold_out": sold_out,
+                    "reviews_amount": reviews_amount,
+                    "category_name": category_name,
+                    "name_site": name_site
+                }
+            except Exception as error:
+                errors[vendor_code] = error
 
     return prices, errors
 
