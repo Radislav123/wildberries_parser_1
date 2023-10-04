7771f45ec040cf1fad0506e8b4026d8a94ccb33d
Vlasov R. V <radislavvlasov@gmail.com>
2023-10-04 21:29:46 +0300

    feat: добавлена логарифмическая зависимость для градиента заливки excel-выгрузки

--- core/service/color.py
+++ core/service/color.py
@@ -1,3 +1,7 @@
+import enum
+import math
+
+
 class Color:
     GREEN = "#6aa84f"
     RED = "#ef6f6f"
@@ -10,13 +14,50 @@ class Color:
 
 
 class XLSXColor(Color):
+    class GradientType(enum.Enum):
+        LINEAR = 0
+        LOG2 = 1
+
     @classmethod
-    def gradient_green(cls, bottom: int, top: int, gradient_bottom: int, gradient_top: int, value: int) -> str:
+    def count_gradient(
+            cls,
+            bottom: int,
+            top: int,
+            gradient_bottom: int,
+            gradient_top: int,
+            gradient_type: "XLSXColor.GradientType",
+            value: int
+    ) -> int:
         if value < bottom:
             value = bottom
         if value > top:
             value = top
-        gradient = int((gradient_top - gradient_bottom) / (top - bottom) * (value - bottom) + gradient_bottom)
+        k = (gradient_top - gradient_bottom) / (top - bottom)
+
+        if gradient_type == cls.GradientType.LINEAR:
+            gradient = int(k * (value - bottom) + gradient_bottom)
+        elif gradient_type == cls.GradientType.LOG2:
+            gradient = int(math.log2((value - bottom) * k) * 32 + gradient_bottom)
+        else:
+            raise ValueError(f"Unknown gradient type: {gradient_type}.")
+
+        if gradient < gradient_bottom:
+            gradient = gradient_bottom
+        if gradient > gradient_top:
+            gradient = gradient_top
+        return gradient
+
+    @classmethod
+    def gradient_green(
+            cls,
+            bottom: int,
+            top: int,
+            gradient_bottom: int,
+            gradient_top: int,
+            gradient_type: "XLSXColor.GradientType",
+            value: int
+    ) -> str:
+        gradient = cls.count_gradient(bottom, top, gradient_bottom, gradient_top, gradient_type, value)
         red = gradient
         green = 255
         blue = gradient
@@ -28,12 +69,16 @@ class XLSXColor(Color):
         return f"#{''.join(color)}"
 
     @classmethod
-    def gradient_red(cls, bottom: int, top: int, gradient_bottom: int, gradient_top: int, value: int) -> str:
-        if value < bottom:
-            value = bottom
-        if value > top:
-            value = top
-        gradient = int((gradient_top - gradient_bottom) / (top - bottom) * (value - bottom) + gradient_bottom)
+    def gradient_red(
+            cls,
+            bottom: int,
+            top: int,
+            gradient_bottom: int,
+            gradient_top: int,
+            gradient_type: "XLSXColor.GradientType",
+            value: int
+    ) -> str:
+        gradient = cls.count_gradient(bottom, top, gradient_bottom, gradient_top, gradient_type, value)
         red = 255
         green = gradient
         blue = gradient
