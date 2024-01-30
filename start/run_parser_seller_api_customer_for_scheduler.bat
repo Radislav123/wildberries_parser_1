ddafff249cd11fcae5c3a5373c36e55c65499ca3
Vlasov R. V <radislavvlasov@gmail.com>
2024-01-30 18:28:11 +0300

    fix: убрана пауза из новых файлов запуска для планировщика

--- start/run_parser_seller_api_customer_for_scheduler.bat
+++ start/run_parser_seller_api_customer_for_scheduler.bat
@@ -1,4 +1,3 @@
 cd ..
 CALL venv\Scripts\activate.bat >> logs\seller_api_customer_production.log 2>&1
 python parse.py seller_api true >> logs\seller_api_customer_production.log 2>&1
-pause
--- start/run_parser_seller_api_other_for_scheduler.bat
+++ start/run_parser_seller_api_other_for_scheduler.bat
@@ -1,4 +1,3 @@
 cd ..
 CALL venv\Scripts\activate.bat >> logs\seller_api_other_production.log 2>&1
 python parse.py seller_api false >> logs\seller_api_other_production.log 2>&1
-pause
