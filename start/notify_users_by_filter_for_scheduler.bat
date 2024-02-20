dfa6abbeb3cd355874db8d132f2e768f04a89006
Vlasov R. V <radislavvlasov@gmail.com>
2024-02-20 18:43:55 +0300

    feat: добавлен функционал оповещения пользователей без токена продавца

--- start/notify_users_by_filter_for_scheduler.bat
+++ start/notify_users_by_filter_for_scheduler.bat
@@ -1,3 +1,4 @@
 cd ..
 CALL venv\Scripts\activate.bat
 python manage.py notify_not_subscribed  >> logs\notify_not_subscribed.log 2>&1
+python manage.py notify_without_seller_api_token  >> logs\notify_without_seller_api_token.log 2>&1
--- start/run_notify_without_seller_api_token_for_scheduler.bat
+++
@@ -1,3 +0,0 @@
-cd ..
-CALL venv\Scripts\activate.bat
-python manage.py notify_without_seller_api_token  >> logs\notify_without_seller_api_token.log 2>&1
