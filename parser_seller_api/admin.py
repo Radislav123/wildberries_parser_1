f5f106bcc773fc6443e76e816f5372b7beb0ef9d
Vlasov R. V <radislavvlasov@gmail.com>
2024-02-26 15:56:55 +0300

    feat: убран лишний фильтр

--- parser_seller_api/admin.py
+++ parser_seller_api/admin.py
@@ -1,30 +1,9 @@
-from django.contrib import admin
-
 from core import admin as core_admin
-from parser_price import models as parser_price_models
 from . import models
 from .settings import Settings
 
 
 settings = Settings()
-
-
-# todo: парсить категорию и сохранять в БД?
-class CategoryFilter(admin.SimpleListFilter):
-    title = "category"
-    parameter_name = "category"
-
-    # todo: прописать типы
-    def lookups(self, request, model_admin):
-        return ((x.id, x) for x in parser_price_models.Category.objects.all())
-
-    # todo: прописать типы
-    def queryset(self, request, queryset):
-        if self.value() is not None:
-            items = parser_price_models.Item.objects.filter(category_id = self.value())
-            print(items)
-            queryset = queryset.filter(vendor_code__in = (x.vendor_code for x in items))
-        return queryset
 
 
 class ParserSellerApiAdmin(core_admin.CoreAdmin):
@@ -34,7 +13,7 @@ class ParserSellerApiAdmin(core_admin.CoreAdmin):
 
 class ItemAdmin(ParserSellerApiAdmin):
     model = models.Item
-    list_filter = (CategoryFilter, "user", "vendor_code")
+    list_filter = ("category", "user", "vendor_code")
     extra_list_display = {"real_price": "category"}
     reorder_fields = {"name_site": "category", "personal_discount": "category"}
 
